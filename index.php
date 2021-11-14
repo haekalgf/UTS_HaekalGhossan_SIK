a803ec8b125c29cfd8f1c08a95eb8d677d2d2344
haekalgf <90390670+haekalgf@users.noreply.github.com>
2021-11-14 23:45:54 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php include_once("berita1.html");?>
+<?php include_once("berita2.html");?>
+<?php include_once("berita3.html");?>
+<?php include_once("dosen.html");?>
+<?php include_once("index.html");?>
+<?php include_once("sejarah.html");?>
+<?php include_once("struktur.html");?>
+<?php include_once("visimisi.html");?>
+<?php include_once("1.png");?>
+<?php include_once("2.png");?>
+<?php include_once("5.png");?>
+<?php include_once("Beautiful design Facebook logo social media PNG - Similar PNG.png");?>
+<?php include_once("diving.png");?>
+<?php include_once("library.jpeg");?>
+<?php include_once("Logo ig, instagram social media icon social media icon set logo #32454 - Free Transparent PNG Logos.png");?>
+<?php include_once("LOGO-UPI.jpg");?>
